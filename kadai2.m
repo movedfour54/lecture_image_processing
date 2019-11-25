14a983ae441b5d272e29b24765ab3fc8705e899a
movedfour54 <30315786+movedfour54@users.noreply.github.com>
2019-11-25 15:51:26 +0900

    Update kadai2.m

--- kadai2.m
+++ kadai2.m
@@ -5,7 +5,7 @@
 
 clear; % 変数のオールクリア
 
-ORG=imread('GirDog.jpg'); % 原画像の入力
+ORG=imread('GirDog.'); % 原画像の入力
 ORG = rgb2gray(ORG); colormap(gray); colorbar;
 imagesc(ORG); axis image; % 画像の表示
 pause; % 一時停止
