5e7f096c44688642a2e5c75fc298db59183469bc
movedfour54 <30315786+movedfour54@users.noreply.github.com>
2019-12-04 00:47:56 +0900

    update済み

--- kadai2.m
+++ kadai2.m
@@ -1,4 +1,4 @@
-% 課題２　階調数と疑似輪郭
+
 % ２階調，４階調，８階調の画像を生成せよ．
 % 下記はサンプルプログラムである．
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
@@ -24,10 +24,13 @@ imagesc(IMG); colormap(gray); colorbar;  axis image;
 pause;
 
 % ８階調画像の生成
-IMG0 = ORG>64;
-IMG1 = ORG>128;
-IMG2 = ORG>192;
-IMG3 = OPG>256;
-IMG = IMG0 + IMG1 + IMG2 + IMG3;
+IMG0 = ORG>32;
+IMG1 = ORG>64;
+IMG2 = ORG>96;
+IMG3 = ORG>128;
+IMG4 = ORG>160;
+IMG5 = ORG>192;
+IMG6 = ORG>224;
+IMG = IMG0 + IMG1 + IMG2 + IMG3 + IMG4 + IMG5 +IMG6;
 imagesc(IMG); colormap(gray); colorbar;  axis image;
 pause;
