f58910360f9558b1131c62ff0786c70ebbcf7904
movedfour54 <30315786+movedfour54@users.noreply.github.com>
2019-12-04 01:06:26 +0900

    update済み

--- kadai2.m
+++ kadai2.m
@@ -1,11 +1,10 @@
-
 % ２階調，４階調，８階調の画像を生成せよ．
 % 下記はサンプルプログラムである．
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 clear; % 変数のオールクリア
 
-ORG=imread('GirDog.png'); % 原画像の入力
+ORG = imread('GirDog.png'); % 原画像の入力
 ORG = rgb2gray(ORG); colormap(gray); colorbar;
 imagesc(ORG); axis image; % 画像の表示
 pause; % 一時停止
